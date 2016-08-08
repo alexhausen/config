57c7e28ec2b5ff22e82cde1f7d1e368733655e0d
Alexandre Hausen <alh@certi.org.br>
2016-08-08 16:22:23 -0300

    [awesome] Use default theme.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -39,7 +39,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("/usr/local/share/awesome/themes/zenburn/theme.lua")
+--beautiful.init("/usr/local/share/awesome/themes/zenburn/theme.lua")
+beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "sakura"
