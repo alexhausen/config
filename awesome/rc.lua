661acf0b0c7d43344b2a46018bfe37995cb359c8
Alexandre Hausen <alex.hausen@gmail.com>
2016-08-12 22:18:15 -0300

    [awesome] theme path

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -39,8 +39,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
---beautiful.init("/usr/local/share/awesome/themes/zenburn/theme.lua")
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "sakura"
