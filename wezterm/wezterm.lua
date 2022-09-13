f09aaed7a3f2c2e01b5c22b2ee75227b4eda3850
Alexandre Hausen <alex.hausen@gmail.com>
2022-09-12 22:23:58 -0300

    Update terminal config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,11 +7,16 @@ return {
   font_size = 14,
   enable_tab_bar = false,
   default_prog = { '/usr/bin/tmux', '-2' },
+  ratelimit_output_bytes_per_second = 4289999998,
   window_padding = {
     top = '0.3cell',
     left = '0.3cell',
     right = 0,
     bottom = 0,
+  },
+  disable_default_key_bindings = true,
+  keys = {
+    { key = 'Backspace', mods= 'CTRL', action = wezterm.action.SendString '\x08'},
   },
   --  color_scheme = "Tango (base16)",
 }
